d884e96df75b1a7b42f5a2241fff535e022f10ed
Toowiredd <155716379+Toowiredd@users.noreply.github.com>
2024-04-28 11:29:12 +1000

    improve

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { createEslintPlugin } from "vite-plugin-eslint";
 
 export default defineConfig({
   server: {
     host: "0.0.0.0",
-    port: "8080",
+    port: 8080,
+    strictPort: true,
+    open: true,
+    cors: true,
   },
-  plugins: [react()],
-  base: "",
+  plugins: [
+    react(),
+    createEslintPlugin({
+      failOnError: true,
+    }),
+  ],
+  base: "/",
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      jsxInject: `import React from 'react'`,
+    },
+  },
 });
