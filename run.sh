3cfdeecd7cada35fb60722ad56b13ba0334ad2de
Toowiredd <155716379+Toowiredd@users.noreply.github.com>
2024-04-28 11:29:10 +1000

    improve

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
-#!/bin/bash
-npm install
-npm run dev
+#!/usr/bin/env bash
+
+# Exit immediately if any command exits with a non-zero status.
+set -e
+
+# Install dependencies and run the dev script using npx.
+npx --quiet npm install && npx --quiet npm run dev
